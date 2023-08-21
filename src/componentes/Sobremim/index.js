227c465b797c027b6e34b42c645c374f3226f699
Luander Silva <98993493+LuanderSilva@users.noreply.github.com>
2023-08-21 13:53:25 -0300

    Add files via upload

--- src/componentes/Sobremim/index.js
+++ src/componentes/Sobremim/index.js
@@ -8,7 +8,7 @@ const Sobremim = () => {
             </div>
 
             <div className='sobremim-conteudo'>
-                <p><span className='space'></span>Olá, tenho 21 anos e desde pequeno fui apaixonado em  ficar na frente da tela do computador. Descobri o mundo do desenvolvimento por volta dos 18 anos, mas resolvi focar de fato na área quando eu tinha 20.</p>
+                <p><span className='space'></span>Olá, tenho 20 anos e desde pequeno fui apaixonado em  ficar na frente da tela do computador. Descobri o mundo do desenvolvimento por volta dos 17 anos, mas resolvi focar de fato na área quando eu tinha 19.</p>
                 <p className='text2'><span className='space'></span>Atualmente estou fazendo faculdade de T.I na Unip e por fora estudo sobre desenvolvimento front-end na Alura. Ainda sou muito novo no ramo e tenho muito a aprender, por esse fato, todos os dias busco consolidar meus conhecimentos através de estudos e praticando o que estou aprendendo constatemente.</p>
                 <p><span className='space'></span>Quando estou sem nada para fazer, gosto de testar minhas habilidades “brincando” um pouco com o código.</p>
             </div>
