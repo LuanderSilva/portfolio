491ac22b48369c0260f611310cb9d5fa51a754b9
Luander Silva <luanderfrota22@gmail.com>
2024-05-16 00:21:41 -0300

    :lipstick: feat: Estilo global

---
+++ src/globalStyle.jsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        background-color: #16161A;
+        width: 100%;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    /* largura */
+
+    .grid-layout {
+        max-width: 1140px;
+        margin: 0 auto;
+    }
+`
