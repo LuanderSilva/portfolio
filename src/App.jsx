4daf8d4ffc072ea7fe2b10085362a020fb2920dc
Luander Silva <luanderfrota22@gmail.com>
2024-05-16 03:58:07 -0300

    :sparkles: Importando os componentes Header e Presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Header from "./componentes/Header/header"
+import Presentation from "./componentes/Presentation/presentation"
 
 function App() {
-  
+  return (
+    <>
+      <Header />
+      <main>
+        <Presentation />
+      </main>
+    </>
+  )
 }
 
 export default App
