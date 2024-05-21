20850afeda35efd56fefad419130d3b9ea077e37
Luander Silva <luanderfrota22@gmail.com>
2024-05-21 02:16:56 -0300

    :sparkles: feat: Componente Footer

---
+++ src/componentes/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Footer() {
+    return (
+        <footer className='footer-container'>
+            <p>
+                Copyright © 2024 - By Luander 🤗
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
