a6553b215856afc37ddfb30bb0927812f816061c
Luander Silva <luanderfrota22@gmail.com>
2024-05-22 01:24:02 -0300

    :sparkles: feat: Englobando as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { GlobalStyle } from './globalStyle.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <AppRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 )
