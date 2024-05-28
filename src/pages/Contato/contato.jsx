70f65d07663d84e418d2031d26b592c1f5a703f5
Luander Silva <luanderfrota22@gmail.com>
2024-05-28 04:54:03 -0300

    :recycle: Passando o alert para o arquivo sendEmail e importando o componente Loading

--- src/pages/Contato/contato.jsx
+++ src/pages/Contato/contato.jsx
@@ -1,9 +1,10 @@
 import "./style.css"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
-import Swal from "sweetalert2"
+import sendEmail from "./sendEmail"
 import Header from "../../componentes/Header/header"
 import Footer from "../../componentes/Footer/footer"
+import Loading from "./Loading/loading"
 
 function Contato({ title }) {
     // ---Título da página---
@@ -14,12 +15,10 @@ function Contato({ title }) {
     // ---Validação---
 
     const { register, handleSubmit, formState: { errors }, reset } = useForm()
-    
-    const submitConsole = () => {
-        Swal.fire({
-            title: "E-mail enviado com sucesso",
-            icon: "success"
-        })
+    const [loading, setLoading] = useState(false);
+
+    const submitConsole = (data) => {
+        sendEmail(data, setLoading)
         reset()
     }
 
@@ -27,57 +26,61 @@ function Contato({ title }) {
         <>
             <Header />
             <main>
-                <section id="s-contact">
-                    <div className="grid-layout">
-                        <div className="contact-title">
-                            <h1>Envie um e-mail</h1>
-                        </div>
+                {loading && <Loading />}
 
-                        <form className="form-container" onSubmit={handleSubmit(submitConsole)}>
-                            <div className="inputs-form">
-                                <div className="campo-nome">
-                                    <input
-                                        {...register("nome", { required: 'Por favor, escreva seu nome' })}
-                                        type="text" name="nome"
-                                        className="input-campo"
-                                        placeholder="nome"
-                                    />
-                                    {errors.nome && <span className="error">{errors.nome.message}</span>}
-                                    <label className="label">Nome</label>
+                {!loading && (
+                    <section id="s-contact">
+                        <div className="grid-layout">
+                            <div className="contact-title">
+                                <h1>Envie um e-mail</h1>
+                            </div>
+
+                            <form className="form-container" onSubmit={handleSubmit(submitConsole)}>
+                                <div className="inputs-form">
+                                    <div className="campo-nome">
+                                        <input
+                                            {...register("nome", { required: 'Por favor, escreva seu nome' })}
+                                            type="text" name="nome"
+                                            className="input-campo"
+                                            placeholder="nome"
+                                        />
+                                        {errors.nome && <span className="error">{errors.nome.message}</span>}
+                                        <label className="label">Nome</label>
+                                    </div>
+
+                                    <div className="campo-email">
+                                        <input
+                                            {...register("email", {
+                                                required: 'Por favor, insira seu e-mail',
+                                            })}
+                                            type="email"
+                                            name="email"
+                                            className="input-campo"
+                                            placeholder="E-mail"
+                                        />
+                                        {errors.email && <span className="error">{errors.email.message}</span>}
+                                        <label className="label">E-mail</label>
+                                    </div>
                                 </div>
 
-                                <div className="campo-email">
-                                    <input
-                                        {...register("email", {
-                                            required: 'Por favor, insira seu e-mail',
+                                <div className="campo-mensagem">
+                                    <textarea
+                                        {...register("msnBox", {
+                                            required: 'Por favor, deixe uma mensagem'
                                         })}
-                                        type="email"
-                                        name="email"
-                                        className="input-campo"
-                                        placeholder="E-mail"
-                                    />
-                                    {errors.email && <span className="error">{errors.email.message}</span>}
-                                    <label className="label">E-mail</label>
+                                        name="msnBox"
+                                        className="msn-box"
+                                        placeholder="Escreva uma mensagem">
+                                    </textarea>
+                                    {errors.msnBox && <span className="error">{errors.msnBox.message}</span>}
+
+                                    <label className="label">Escreva uma mensagem</label>
+                                    <input type="submit" value="Enviar" className="button-form" />
                                 </div>
-                            </div>
-
-                            <div className="campo-mensagem">
-                                <textarea
-                                {...register("msnBox",{
-                                    required: 'Por favor, deixe uma mensagem'
-                                })}
-                                    name="msnBox"
-                                    className="msn-box"
-                                    placeholder="Escreva uma mensagem">
-                                </textarea>
-                                {errors.msnBox && <span className="error">{errors.msnBox.message}</span>}
-
-                                <label className="label">Escreva uma mensagem</label>
-                                <input type="submit" value="Enviar" className="button-form" />
-                            </div>
-                        </form>
-                    </div>
-                </section>
+                            </form>
+                        </div>
+                    </section>
+                )}
             </main>
             <div className="footer">
                 <Footer />
